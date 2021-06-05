b7624677d8463df8197649ffc2818630b65ed39f
radiotae <radiotae@gmail.com>
2021-06-05 17:40:24 +0900

    Add project files.

---
+++ TetrisPlusPlus/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TetrisPlusPlus
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TetrisPlusPlus())
+                game.Run();
+        }
+    }
+}
